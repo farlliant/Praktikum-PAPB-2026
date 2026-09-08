22a68f632c0b6c355afe3654e81d809770b8a901
farlliant <farrellbrilliantt@gmail.com>
2026-09-08 13:47:26 +0700

    feat(modul2): implement Box layout experiment

--- Modul 2/app/src/main/java/com/example/modul2_245150201111053_farrelbrilliant/MainActivity.kt
+++ Modul 2/app/src/main/java/com/example/modul2_245150201111053_farrelbrilliant/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
@@ -24,12 +25,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
 
-                    Row(
+                    Box(
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        Text("Nama: Andi")
-                        Text(" | ")
-                        Text("NIM: 215150201111000")
+                        Text("Teks di belakang")
+
+                        Button(onClick = {}) {
+                            Text("Tombol di depan")
+                        }
                     }
                 }
             }
