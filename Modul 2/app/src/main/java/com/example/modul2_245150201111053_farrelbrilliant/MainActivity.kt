f9861daca9e355cb1eb9b71581307b0b5d395829
farlliant <farrellbrilliantt@gmail.com>
2026-09-08 13:50:00 +0700

    feat(modul2): implement Follow Unfollow state

--- Modul 2/app/src/main/java/com/example/modul2_245150201111053_farrelbrilliant/MainActivity.kt
+++ Modul 2/app/src/main/java/com/example/modul2_245150201111053_farrelbrilliant/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import com.example.modul2_245150201111053_farrelbrilliant.ui.theme.Modul2_245150201111053_FarrelBrilliantTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,24 +29,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
 
-                    Column(
+                    FollowButton(
                         modifier = Modifier.padding(innerPadding)
-                    ) {
-
-                        Text(
-                            text = "Dengan Modifier",
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .size(200.dp, 60.dp)
-                                .background(Color.Yellow)
-                        )
-
-                        Text(
-                            text = "Tanpa Modifier"
-                        )
-                    }
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun FollowButton(modifier: Modifier = Modifier) {
+
+    var isFollowed by remember {
+        mutableStateOf(false)
+    }
+
+    Button(
+        onClick = {
+            isFollowed = !isFollowed
+        },
+        modifier = modifier
+    ) {
+        Text(
+            text = if (isFollowed) {
+                "Unfollow"
+            } else {
+                "Follow"
+            }
+        )
     }
 }
