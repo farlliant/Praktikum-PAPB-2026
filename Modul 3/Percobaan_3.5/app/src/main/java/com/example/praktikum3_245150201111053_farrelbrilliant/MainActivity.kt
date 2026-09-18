8f2f88b4ca2eb125e51b0ae6738f30f1c59c7ec3
farlliant <farrellbrilliantt@gmail.com>
2026-09-18 18:01:29 +0700

    feat(modul3): implement toggle state experiment

--- Modul 3/Percobaan_3.5/app/src/main/java/com/example/praktikum3_245150201111053_farrelbrilliant/MainActivity.kt
+++ Modul 3/Percobaan_3.5/app/src/main/java/com/example/praktikum3_245150201111053_farrelbrilliant/MainActivity.kt
@@ -4,24 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum3_245150201111053_farrelbrilliant.ui.theme.Praktikum3_245150201111053_FarrelBrilliantTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             Praktikum3_245150201111053_FarrelBrilliantTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
+                    FollowButton(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun FollowButton(modifier: Modifier = Modifier) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum3_245150201111053_FarrelBrilliantTheme {
-        Greeting("Android")
+    var isFollowed by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Button(
+            onClick = {
+                isFollowed = !isFollowed
+            }
+        ) {
+
+            Text(
+                text = if (isFollowed) {
+                    "Unfollow"
+                } else {
+                    "Follow"
+                }
+            )
+        }
     }
 }
